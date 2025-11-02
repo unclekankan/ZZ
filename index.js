fa151c6df6d385dcea5ad3b6381cda7829f4a7f8
unclekankan <2591814909@qq.com>
2025-11-03 00:56:27 +0800

    添加固定导航栏

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const dt = document.querySelector('.dt')
 window.addEventListener('scroll', function () {
   const nt = document.documentElement.scrollTop
   if (nt >= 100) {
+    document.querySelector('.top').style.transform = 'translateY(0)'
     dt.style.display = 'block'
-  } else { dt.style.display = 'none' }
+  } else {
+    dt.style.display = 'none'
+    document.querySelector('.top').style.transform = 'translateY(-100px)'
+  }
 })
 dt.addEventListener('click', function () {
   document.documentElement.scrollTop = 0
